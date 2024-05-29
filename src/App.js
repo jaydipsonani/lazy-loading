112a603b206167e5bc8e1df4d1b9100e015eba93
sonanijaydip <sonanijaydip4@gmmail.com>
2024-05-29 18:35:07 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Component2 />
       </Suspense>
       <h1>hello</h1>
-      <h1>the new hello branch</h1>
+      <h1>the new hello branch (hello branch)</h1>
     </div>
   );
 }
