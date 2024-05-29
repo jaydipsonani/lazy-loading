8b4daabcf9b72cc0f28064cdd62b3c96e4992508
sonanijaydip <sonanijaydip4@gmmail.com>
2024-05-29 16:27:54 +0530

    lazy-loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 // import Component1 from './Component1';
 // import Component2 from './Component2';
+// import Component3 from './Component3';
 import { Suspense, lazy } from 'react';
 const Component1 = lazy(() => import('./Component1'))
 const Component2 = lazy(() => import('./Component2'))
+// const Component3 = lazy(() => import('./Component3'))
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
       <Suspense fallback={<div>Loading...component2</div>}>
         <Component2 />
       </Suspense>
+      {/* <Suspense fallback={<div>Loading...component3</div>}>
+        <Component3/>
+      </Suspense> */}
     </div>
   );
 }
